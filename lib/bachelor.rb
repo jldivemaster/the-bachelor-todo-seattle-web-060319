ac93e127f68f745f37dd81de0da8b05042f736e9
Jason Leach <jlspursfan@gmail.com>
2019-05-22 03:45:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,22 +16,31 @@ def get_first_name_of_season_winner(data, season)
   end
 end
 
+
 def get_contestant_name(data, occupation)
   data.each do |k, array|
     array.each do |lady|
       lady.each do |stat_id, stat_val|
+        if lady["occupation"] == occupation
+          return lady["name"]
+        end
       end
     end
   end
 end
 
 def count_contestants_by_hometown(data, hometown)
+  count = 0
   data.each do |k, array|
     array.each do |lady|
       lady.each do |stat_id, stat_val|
+        if lady["hometown"] == hometown
+          count += 1
+        end
       end
     end
   end
+  return count
 end
 
 def get_occupation(data, hometown)
