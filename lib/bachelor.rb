d3cdf7ff7e24dc10596b5b0c3b655799aec181d3
Jason Leach <jlspursfan@gmail.com>
2019-05-22 03:38:27 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,19 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data.each do |k, array|
+    if k == season
+      array.each do |lady|
+        lady.each do |stat_id, stat_val|
+          first_name = ""
+          if stat_id == "name"
+            name_arr = lady["name"].split(" ")
+            first_name += name_arr[0]
+            return first_name if stat_val == "Winner"
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
