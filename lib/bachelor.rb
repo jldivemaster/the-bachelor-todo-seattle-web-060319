9e2759766c5737711d3a42993d8b075a7fb49740
Jason Leach <jlspursfan@gmail.com>
2019-05-22 03:42:51 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,17 +17,37 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |k, array|
+    array.each do |lady|
+      lady.each do |stat_id, stat_val|
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.each do |k, array|
+    array.each do |lady|
+      lady.each do |stat_id, stat_val|
+      end
+    end
+  end
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |k, array|
+    array.each do |lady|
+      lady.each do |stat_id, stat_val|
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data.each do |k, array|
+    array.each do |lady|
+      lady.each do |stat_id, stat_val|
+      end
+    end
+  end
 end
